49ad7c88b2b65f5405797560ddaebf5b1eac12d4
Kulbach <polina.alexia@gmail.com>
2017-11-22 20:03:17 +0200

    Lab_8

--- js/comment.js
+++ js/comment.js
@@ -1,10 +1,50 @@
 $(document).ready(function (){
+	s = localStorage;
+	if (s.getItem('comment1')) {
+		for (var i = 0; i < s.length; i++) {
+				key = s.key(i);
+				if(~key.indexOf("comment")){
+							id = key.substr(7);
+							comment = s.getItem("comment" +id);
+							var date = new Date();
+							$("#comments").prepend("<div class='container bod'><h2>Вася Пупкін  <time>" + date.getDate() + "." + (date.getMonth()+1) +"." + date.getFullYear() + "</time></h2>"  + comment + "</div>");
+	}
+}}
+
+window.addEventListener('online', function(e) {
+	k = false; 
+	for (var i = 0; i < s.length; i++) {
+				key = s.key(i);
+				if(~key.indexOf("comment")){
+					k = true;
+				}
+			}
+				if(k){
+					alert("Відправляю дані с LS на север");}
+				
+});
+
 	$(document).on("click", "#add_comment", function (){
 		if($("#comment").val()!=""){
 			var comment = $("#comment").val();
 			var date = new Date();
-			$("#comments").prepend("<div class='container bod'><h2>Вася Пупкін  <time>" + date.getDate() + "." + (date.getMonth()+1) +"." + date.getFullYear() + "</time></h2>"  + comment + "</div>")
-			$("#comment").val("");}}
+		if(navigator.onLine){
+			alert("Відправляю на сервер і читаю з сервера");
+			$("#comments").prepend("<div class='container bod'><h2>Вася Пупкін  <time>" + date.getDate() + "." + (date.getMonth()+1) +"." + date.getFullYear() + "</time></h2>"  + comment + "</div>");
+		}
+		else{
+			n =0;
+					for (var i = 0; i < s.length; i++) {
+						key = s.key(i);
+							if(~key.indexOf("comment")){
+								n++;
+							}
+					}
+				localStorage.setItem("comment"+(n+1), comment);
+				$("#comments").prepend("<div class='container bod'><h2>Вася Пупкін  <time>" + date.getDate() + "." + (date.getMonth()+1) +"." + date.getFullYear() + "</time></h2>"  + comment + "</div>");
+		}
+		$("#comment").val("");
+		}
 	});
 	
 });
--- js/news.js
+++ js/news.js
@@ -1,3 +1,4 @@
+window.k = 0;
 $(document).ready(function (){
 function readURL(input) {
     if (input.files && input.files[0]) {
@@ -9,9 +10,21 @@ function readURL(input) {
 				reader.readAsDataURL(input.files[0]);
     }
 }
+function readImage(fileInput, n) { //Загрузка изображения
+          if ( fileInput.files && fileInput.files[0] ) {
+            var FR= new FileReader();
+            FR.onload = function(e) {
+              localStorage.setItem('newsImage' + (n+1), e.target.result); 
+            };       
+            FR.readAsDataURL( fileInput.files[0] );
+          }
+        }
 $("input[type=file]").change(function(){
     readURL(this);
 });
+function isOnline() {
+    return window.navigator.onLine; 
+}
 
 $(document).on("click", "#send_news", function (){
 
@@ -46,7 +59,21 @@ $(document).on("click", "#send_news", function (){
 			if(error!=true){
 				document.getElementById("descr").style.borderColor="grey";
 				document.getElementById("title").style.borderColor = "grey";
-				alert("Новина успішно опублікована!");
+			if(navigator.onLine){
+				alert("Кидаю на сервер і читаю з сервера");}
+				else {
+					n =0;
+					s = localStorage;
+					for (var i = 0; i < s.length; i++) {
+						key = s.key(i);
+							if(~key.indexOf("newsTitle")){
+								n++;
+							}
+					}
+						readImage($("#image_file")[0], n);
+						localStorage.setItem("newsTitle" + (n+1), $("#title").val());
+						localStorage.setItem("newsDescr" + (n+1), $("#descr").val());
+				}
 				$('#img').attr('src', "img/camera.png");
 				$("#descr").val("");
 				$("#title").val("");
